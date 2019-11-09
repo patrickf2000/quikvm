78032297ba0321dafa27a052beedc4d804bd3b81
patrick <dev@patrickflynn.co>
2019-11-09 17:11:52 -0500

    added command line arguments for controlling monitoring

--- vm/main.cpp
+++ vm/main.cpp
@@ -10,19 +10,29 @@ int main(int argc, char *argv[]) {
 		return 1;
 	}
 	
+	bool t_monitor = false;
+	
+	for (int i = 1; i<argc; i++) {
+		if (std::string(argv[i]) == "-t" || std::string(argv[i]) == "--time") {
+			t_monitor = true;
+		}
+	}
+	
 	auto start = std::chrono::steady_clock::now();
 	
 	load(argv[1]);
 	run_start();
 	
-	auto end = std::chrono::steady_clock::now();
-	auto diff = end - start;
-	auto elapsed = std::chrono::duration <double, std::milli> (diff).count();
-	
-	std::cout << std::endl;
-	std::cout << "Program execution time: " << std::endl;
-	std::cout << elapsed << " ms" << std::endl;
-	std::cout << (elapsed/1000) << " secs" << std::endl;
+	if (t_monitor) {
+		auto end = std::chrono::steady_clock::now();
+		auto diff = end - start;
+		auto elapsed = std::chrono::duration <double, std::milli> (diff).count();
+		
+		std::cout << std::endl;
+		std::cout << "Program execution time: " << std::endl;
+		std::cout << elapsed << " ms" << std::endl;
+		std::cout << (elapsed/1000) << " secs" << std::endl;
+	}
 	
 	return 0;
 }
