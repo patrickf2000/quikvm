88bf696932b53b9a08b9dde78e2f06263baf88f6
patrick <dev@patrickflynn.co>
2019-11-08 20:36:55 -0500

    made slight changes to library naming

--- vm/loader.cpp
+++ vm/loader.cpp
@@ -6,13 +6,13 @@
 
 void excall(std::string cmd) {
 	int pos = cmd.find(">");
-	auto lib = cmd.substr(0, pos);
+	auto lib = "libqk" + cmd.substr(0, pos) + ".so";
 	auto func_name = cmd.substr(pos+1);
 	
 	void *handle;
 	void (*func)(void);
 	
-	handle = dlopen("libqkstdlib.so", RTLD_LAZY);
+	handle = dlopen(lib.c_str(), RTLD_LAZY);
 	if (!handle) {
 		std::cout << "Error: Unable to load API" << std::endl;
 		std::exit(1);
