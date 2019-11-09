4b644169306cd8b5793dd64491d894055197919b
patrick <dev@patrickflynn.co>
2019-11-08 20:46:02 -0500

    added support for specifying datatype

--- test/test_lib.asm
+++ test/test_lib.asm
@@ -1,6 +1,9 @@
 s_load "Testing an external function:"
 s_print
 
+i_load 5
+
 excall "test>test1"
+excall "test>test2:int"
 
 exit
--- vm/loader.cpp
+++ vm/loader.cpp
@@ -7,7 +7,15 @@
 void excall(std::string cmd) {
 	int pos = cmd.find(">");
 	auto lib = "libqk" + cmd.substr(0, pos) + ".so";
+	
+	std::string type = "NULL";
 	auto func_name = cmd.substr(pos+1);
+	
+	int dpos = func_name.find(":");
+	if (dpos != std::string::npos) {
+		type = func_name.substr(dpos+1);
+		func_name = cmd.substr(pos+1, dpos);
+	}
 	
 	void *handle;
 	void (*func)(void);
