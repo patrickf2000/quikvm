4172ee9ba76e8530302d9446d213c1b5c9906fb9
patrick <dev@patrickflynn.co>
2019-11-14 08:55:57 -0500

    updated str part of library to use new writer functions

--- libbackend/str.c
+++ libbackend/str.c
@@ -6,16 +6,9 @@
 
 void qkbk_print_str(char *str) 
 {
-	char *ln1 = "s_load \"";
-	char *ln2 = "\"\n";
-	char *ln3 = "s_print\n";
-	char *ln4 = "s_pop\n";
-	
-	fwrite(ln1, sizeof(char), strlen(ln1), fp);
-	fwrite(str, sizeof(char), strlen(str), fp);
-	fwrite(ln2, sizeof(char), strlen(ln2), fp);
-	fwrite(ln3, sizeof(char), strlen(ln3), fp);
-	fwrite(ln4, sizeof(char), strlen(ln4), fp);
+	qkbk_write_str("s_load \"");
+	qkbk_write_str(str);
+	qkbk_write_str("\"\ns_print\ns_pop\n");
 }
 
 void qkbk_print_nl() 
